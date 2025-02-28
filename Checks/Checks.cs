9066044c328f2dc521c521cc56bb03f55afaf656
Mumlul <al.ploskikh@list.ru>
2025-02-28 21:11:30 +0500

    send_code_to_email

--- Checks/Checks.cs
+++ Checks/Checks.cs
@@ -21,7 +21,16 @@ namespace Checks
             string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
             return Regex.IsMatch(email, pattern);
         }
+        //Проверкак кода аутентификации с почты
+        static public bool IsValidSecretpassword(string code,string usercode)
+        { 
+            if (string.IsNullOrEmpty(code))
+                return false;
+
+            return code.Equals(usercode, StringComparison.Ordinal);
+        }
     }
+    
 
     //Методы Регистрации
     public class HelpRegister
@@ -43,52 +52,6 @@ namespace Checks
             return new string(Enumerable.Repeat(charPool, length)
                 .Select(s => s[random.Next(s.Length)]).ToArray());
         }
-
-        //2факторная аутентификация
-        static public async Task SendSecretKeyToEmailAsync(string email)
-        {
-            
-            try
-            {
-                string smtpServer = "smtp.mail.ru";
-                int smtpPort = 587; 
-                string senderEmail = "a.ploskikh@list.ru"; 
-                string senderPassword = "dRusv8QTzTAkxHWLZBMp";
-                string secretKey = GeneratePassword(6,true,true);
-                // Создание сообщения
-                MailMessage mail = new MailMessage();
-                mail.From = new MailAddress(senderEmail);
-                mail.To.Add(email);
-                mail.Subject = "Секретный ключ для двухфакторной аутентификации";
-                mail.Body = $"Ваш секретный ключ: {secretKey}\n" +
-                            $"Вы можете импортировать его в приложение для аутентификации.";
-
-                // Настройка SMTP-клиента
-                SmtpClient smtpClient = new SmtpClient(smtpServer)
-                {
-                    Port = smtpPort,
-                    Credentials = new NetworkCredential(senderEmail, senderPassword),
-                    EnableSsl = true, 
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    Timeout = 10000
-                };
-
-                // Асинхронная отправка письма
-                await smtpClient.SendMailAsync(mail);
-            }
-            catch (SmtpException ex)
-            {
-                
-                Console.WriteLine($"Ошибка SMTP: {ex.Message}. Код состояния: {ex.StatusCode}");
-                throw; // Переброс исключения дальше
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Произошла ошибка: {ex.Message}");
-                throw; // Переброс исключения дальше
-            }
-        }
-
     }
 
 }
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +16,16 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
            /* MessageBox.Show("Hello World!");*/
             InitializeComponent();
         }
+
+        public string secretcode="";
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -31,19 +37,61 @@ namespace WindowsFormsApp1
         {
             try
             {
-                await HelpRegister.SendSecretKeyToEmailAsync("al.ploskikh@list.ru"); 
-
+                await SendSecretKeyToEmailAsync("al.ploskikh@list.ru"); 
                 MessageBox.Show("Секретный ключ успешно отправлен на почту.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (SmtpException ex)
             {
-                // Обработка ошибок SMTP
                 MessageBox.Show($"Ошибка SMTP: {ex.Message}. Код состояния: {ex.StatusCode}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             catch (Exception ex)
             {
-                // Обработка остальных ошибок
                 MessageBox.Show($"Произошла ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        //Он должен быть тут потому тчо если его засунуть в библиотеку то он перестает работать 
+        public async Task SendSecretKeyToEmailAsync(string email)
+        {
+
+            try
+            {
+                string smtpServer = "smtp.mail.ru";
+                int smtpPort = 587;
+                string senderEmail = "a.ploskikh@list.ru";
+                string senderPassword = "dRusv8QTzTAkxHWLZBMp";
+                string secretKey = HelpRegister.GeneratePassword(10,true,true);
+                secretcode = secretKey;
+                // Создание сообщения
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(senderEmail);
+                mail.To.Add(email);
+                mail.Subject = "Секретный ключ для двухфакторной аутентификации";
+                mail.Body = $"Ваш секретный ключ: {secretKey}\n" +
+                            $"Вы можете импортировать его в приложение для аутентификации.";
+
+                // Настройка SMTP-клиента
+                SmtpClient smtpClient = new SmtpClient(smtpServer)
+                {
+                    Port = smtpPort,
+                    Credentials = new NetworkCredential(senderEmail, senderPassword),
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Timeout = 10000
+                };
+
+                // Асинхронная отправка письма
+                await smtpClient.SendMailAsync(mail);
+            }
+            catch (SmtpException ex)
+            {
+
+                Console.WriteLine($"Ошибка SMTP: {ex.Message}. Код состояния: {ex.StatusCode}");
+                throw; // Переброс исключения дальше
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                throw; // Переброс исключения дальше
             }
         }
     }
