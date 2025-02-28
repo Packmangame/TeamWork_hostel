2247217b0f8aea83b7cf328ae21137e8a5dde0ba
Packmangame <157449270+Packmangame@users.noreply.github.com>
2025-02-28 10:51:19 +0500

    First try connect

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,17 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int num = rand.Next(1, 100);
+            label1.Text = num.ToString();
+        }
     }
 }
